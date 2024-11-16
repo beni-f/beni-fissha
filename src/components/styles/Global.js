4e89494980292e5e812cffde0d72dbcc64d55ed0
beni-f <benifissha@gmail.com>
2024-11-17 00:18:45 +0300

    updated

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -1,6 +1,11 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
+
+    * {
+        box-sizing: border-box;
+    }
+
     body {
         font-family: 'Roboto', sans-serif;
         background: linear-gradient(90.6deg, rgb(245, 239, 249) 30.1%, rgb(207, 211, 236) 100.2%);
